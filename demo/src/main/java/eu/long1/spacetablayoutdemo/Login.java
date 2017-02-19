89c251b3646026a24d681e61bc02bffe01df978d
PutraNasri <nasrisyawaldi@gmail.com>
2017-02-19 23:34:17 +0700

    190217

---
+++ demo/src/main/java/eu/long1/spacetablayoutdemo/Login.java
@@ -0,0 +1,21 @@
+package eu.long1.spacetablayoutdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.HashMap;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void login(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
